daa3d201aa32252daa742073ac9d43910152e130
Sai Krishna <asaikrishna.op@gmail.com>
2023-07-04 10:52:01 +0530

    first commit

---
+++ amazon.js
@@ -0,0 +1,7 @@
+function goToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+const bu= document.getElementById('return');
+bu.addEventListener('click',function(){
+  window.location.href='Amazon-return.html';
+});
